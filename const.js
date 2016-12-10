4386412806093ea004b9f3bdcd42427916c8b19f
avinashsivaraman <avinashcegian95@gmail.com>
2016-12-10 12:20:03 +0530

    Initial setup

--- const.js
+++ const.js
@@ -2,20 +2,17 @@
 
 // Wit.ai parameters
 const WIT_TOKEN = process.env.WIT_TOKEN;
-if (!WIT_TOKEN) {
-  throw new Error('missing WIT_TOKEN');
-}
 
 // Messenger API parameters
 const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 
 var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 if (!FB_VERIFY_TOKEN) {
-  FB_VERIFY_TOKEN = "just_do_it";
+  FB_VERIFY_TOKEN = "DualCoreMonk";
 }
 
 module.exports = {
-  WIT_TOKEN: WIT_TOKEN,
-  FB_PAGE_TOKEN: FB_PAGE_TOKEN,
+  WIT_TOKEN: "WWLZSGQQWHB7ZJTAR35DOSVCD6VHZWMS",
+  FB_PAGE_TOKEN: "EAAFYRancT9YBAKLNdqWXY2WUsDE581GU2X1mGRxQkC1AR4r8rNcgsvVZBT2F4gjWRxLGz6t7HlIOk7H0dpcZB6ujcZBIJxC76zBJcrgZC3PJ139OZCSX0CkxLDq05y8tSnZCzduFueTyiq7lEcHMSdFZCZAgXZBt7BXHNJIjljmcSgQZDZD",
   FB_VERIFY_TOKEN: FB_VERIFY_TOKEN,
 };
